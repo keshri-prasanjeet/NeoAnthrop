f1aebe3f32940ccd4d88b50a31ff8e459a04ed1e
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2025-02-02 13:39:17 +0530

    added npm packages and initialized openapi generation using swagger json

---
+++ neoanthrop-ui/src/app/services/apis/index.ts
@@ -0,0 +1,5 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './ChatApi';
+export * from './MessageApi';
+export * from './UserApi';
---
+++ neoanthrop-ui/src/app/services/models/index.ts
@@ -0,0 +1,6 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './MessageRequest';
+export * from './MessageResponse';
+export * from './StringResponse';
+export * from './UserResponse';
