06cb2864fbddda78d5d93c2ed01134a4ee61445e
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2025-02-07 09:19:50 +0530

    reset of the ui

--- neoanthrop-ui/src/app/services/models/index.ts
+++
@@ -1,6 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-export * from './MessageRequest';
-export * from './MessageResponse';
-export * from './StringResponse';
-export * from './UserResponse';
--- neoanthrop-ui/src/app/utils/keycloak/keycloakService.ts
+++
@@ -1,8 +0,0 @@
-import Keycloak from "keycloak-js";
-
-const keycloakConfig = {
-    url: "https://d930-2401-4900-1f25-626b-f121-20e8-b018-d1fa.ngrok-free.app",
-    realm: "neo-anthrop",
-    clientId: "neo-anthrop-app"
-};
-const keycloak = new Keycloak(keycloakConfig);
